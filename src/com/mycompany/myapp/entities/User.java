258be4d291f3506b9e849a10a229efc6ec0aa33a
tazarkour <jaouaniw@gmail.com>
2022-02-28 15:42:00 +0100

    done the json stuff

---
+++ src/com/mycompany/myapp/entities/User.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.myapp.entities;
+
+/**
+ *
+ * @author bhk
+ */
+public class User {
+    private int id,points;
+    private String username,email,photo,bio,password;
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    private boolean is_verified;
+
+    public User(int id, int points, String username, String email, String photo, String bio, boolean is_verified,String Pwd) {
+        this.id = id;
+        this.points = points;
+        this.username = username;
+        this.email = email;
+        this.photo = photo;
+        this.bio = bio;
+        this.is_verified = is_verified;
+        this.password= Pwd;
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    
+     public User(int points, String username, String email, String photo, String bio, boolean is_verified,String Pwd) {
+        this.id = id;
+        this.points = points;
+        this.username = username;
+        this.email = email;
+        this.photo = photo;
+        this.bio = bio;
+        this.is_verified = is_verified;
+        this.password= Pwd;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", points=" + points + ", username=" + username + ", email=" + email + ", photo=" + photo + ", bio=" + bio + ", is_verified=" + is_verified + ", Password= "+password+ '}';
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public void setIs_verified(boolean is_verified) {
+        this.is_verified = is_verified;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public int getPoints() {
+        return points;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public boolean isIs_verified() {
+        return is_verified;
+    }
+    
+    
+    
+    
+}
---
+++ src/com/mycompany/myapp/services/ServiceUser.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.myapp.services;
+
+import com.codename1.io.CharArrayReader;
+import com.codename1.io.ConnectionRequest;
+import com.codename1.io.JSONParser;
+import com.codename1.io.NetworkEvent;
+import com.codename1.io.NetworkManager;
+import com.codename1.ui.events.ActionListener;
+import com.mycompany.myapp.entities.User;
+import com.mycompany.myapp.utils.Statics;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author bhk
+ */
+public class ServiceUser {
+
+    public ArrayList<User> Users;
+    
+    public static ServiceUser instance=null;
+    public boolean resultOK;
+    private ConnectionRequest req;
+
+    private ServiceUser() {
+         req = new ConnectionRequest();
+    }
+
+    public static ServiceUser getInstance() {
+        if (instance == null) {
+            instance = new ServiceUser();
+        }
+        return instance;
+    }
+
+    public boolean addUser(User U) {
+        System.out.println(U);
+        System.out.println("********");
+       //String url = Statics.BASE_URL + "create?name=" + t.getName() + "&status=" + t.getStatus();
+       String url = Statics.BASE_URL + "create";
+    
+       req.setUrl(url);
+       
+       req.addArgument("username", U.getUsername());
+       req.addArgument("password", U.getPassword());
+       req.addArgument("email", U.getEmail());
+       req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                resultOK = req.getResponseCode() == 200; //Code HTTP 200 OK
+                req.removeResponseListener(this);
+            }
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return resultOK;
+    }
+
+    public ArrayList<User> parseUsers(String jsonText){
+        try {
+            Users=new ArrayList<>();
+            JSONParser j = new JSONParser();
+            Map<String,Object> tasksListJson = 
+               j.parseJSON(new CharArrayReader(jsonText.toCharArray()));
+            
+            List<Map<String,Object>> list = (List<Map<String,Object>>)tasksListJson.get("root");
+            for(Map<String,Object> obj : list){
+                User U = new User();
+              // int id = Integer.parseInt(obj.get("id").toString());
+                //U.setId((int)id);
+                U.setId(((int)Float.parseFloat(obj.get("id").toString())));
+                U.setUsername(obj.get("username").toString());
+                U.setEmail(obj.get("email").toString());
+                if (obj.get("Bio")==null)
+                     U.setBio("Null");
+               else
+                     U.setBio(obj.get("Bio").toString());
+                //U.setPassword(obj.get("password").toString());
+               // int points=(((int)Float.parseFloat(obj.get("points").toString())));
+                //if (points==null)
+                  //  U.setPoints(0);
+                //else
+                U.setPoints(((int)Float.parseFloat(obj.get("points").toString())));
+                Users.add(U);
+            }
+            
+            
+        } catch (IOException ex) {
+            
+        }
+        return Users;
+    }
+    
+    public ArrayList<User> getAllUsers(){
+        //String url = Statics.BASE_URL+"/tasks/";
+        String url = Statics.BASE_URL+"getusersmobile/";
+        System.out.println(url);
+        req.setUrl(url);
+        req.setPost(false);
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                Users = parseUsers(new String(req.getResponseData()));
+                req.removeResponseListener(this);
+            }
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return Users;
+    }
+}
